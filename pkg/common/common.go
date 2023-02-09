7eadd8d2a999e06f6f446840dc5ac0a30d77e7c4
Kumar Yadav <Adarsh.KumarYadav@dell.com>
2023-02-09 13:46:13 +0530

    decreasing timeOut

--- pkg/common/common.go
+++ pkg/common/common.go
@@ -425,7 +425,7 @@ func SetAPIPort(ctx context.Context) {
 // ReachableEndPoint checks if this endpoint is reachable or not
 func ReachableEndPoint(endpoint string) bool {
 	// this endpoint has IP:PORT
-	_, err := net.DialTimeout("tcp", endpoint, Timeout)
+	_, err := net.DialTimeout("tcp", endpoint, 2*time.Second)
 	if err != nil {
 		return false
 	}
