290f0884a9d99c1e8d0c097def11d7c59f29d45e
abhi16394 <32352976+abhi16394@users.noreply.github.com>
2024-03-27 15:19:31 -0700

    Updating version support (#321)

--- dell-csi-helm-installer/verify-csi-powerstore.sh
+++ dell-csi-helm-installer/verify-csi-powerstore.sh
@@ -10,8 +10,8 @@
 
 # verify-csi-powerstore method
 function verify-csi-powerstore() {
-  verify_k8s_versions "1.24" "1.28"
-  verify_openshift_versions "4.12" "4.13"
+  verify_k8s_versions "1.24" "1.29"
+  verify_openshift_versions "4.13" "4.14"
   verify_namespace "${NS}"
   verify_required_secrets "${RELEASE}-config"
   verify_alpha_snap_resources
