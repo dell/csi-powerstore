183869e9f81f432e57158a23b9cdbc0546c1f16a
Boya Murthy <boya.murthy@dell.com>
2024-05-08 14:43:59 +0530

    Adding 10.2 changes

--- dell-csi-helm-installer/verify-csi-powerstore.sh
+++ dell-csi-helm-installer/verify-csi-powerstore.sh
@@ -11,7 +11,7 @@
 # verify-csi-powerstore method
 function verify-csi-powerstore() {
   verify_k8s_versions "1.24" "1.29"
-  verify_openshift_versions "4.13" "4.14"
+  verify_openshift_versions "4.13" "4.15"
   verify_namespace "${NS}"
   verify_required_secrets "${RELEASE}-config"
   verify_alpha_snap_resources
