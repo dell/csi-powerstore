952229da3ed4a3c55e3e2924d5401008fafa8a38
Lau, Luke <luke.lau1@dell.com>
2024-10-14 11:44:37 -0400

    improve output formatting

--- tests/e2e/driver/metro-e2e.sh
+++ tests/e2e/driver/metro-e2e.sh
@@ -13,28 +13,59 @@
 #  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
-# set -x
 
-red_text="\033[31m"
-green_text="\033[32m"
-reset_text="\033[0m"
+script_path=$(dirname "${BASH_SOURCE[0]}")
 
+# script default options
 key_path=""
 username=$USER
 
+# test resources
+test_resources_path="$script_path/resources"
+test_resource_sc="sc.yaml"
+test_resource_deployment="deployment.yaml"
 pod_name=powerstoretest-0
 pvc_name="pvol0"
 volume_mount_path="/data0"
 volume_prefix="csivol-"
 namespace=testpowerstore
 
+# test timeouts
 timeout_seconds=120
 sleep_interval=2
+
+# color codes
+red_text="\033[31m"
+green_text="\033[32m"
+yellow_text="\033[33m"
+reset_text="\033[0m"
+
+# Function to handle SIGINT (Ctrl+C)
+exit_script() {
+    echo "Cleaning and exiting script..."
+    clean_test_env
+    exit 0
+}
+
+# Register the trap for SIGINT
+trap "exit_script" SIGINT
 
 # print a message with a preceding newline
 function print() {
     local message=$1
-    echo -e "\n[$(date)]\n$message"
+    echo -e "\n[$( date +"%Y-%m-%dT%H:%M:%S.%3N")] $message"
+}
+
+function print_fail() {
+    print "${red_text}FAIL: $1${reset_text}"
+}
+
+function print_pass() {
+    print "${green_text}PASS: $1${reset_text}"
+}
+
+function print_notice() {
+    print "${yellow_text}$1${reset_text}"
 }
 
 # use the exit code of the last command to report success or failure
@@ -43,9 +74,9 @@ function pass_fail() {
     local test_name=$2
 
     if [[ "$exit_code" -eq 0 ]]; then
-        print "${green_text}PASS: $test_name${reset_text}"
+        print_pass "$test_name"
     else 
-        print "${red_text}FAIL: $test_name${reset_text}"
+        print_fail "$test_name"
     fi
 }
 
@@ -77,17 +108,17 @@ function run_test() {
 # deploy the storage class and test environment.
 # the test environment consists of a pod, pvc, stateful set, and service account.
 function deploy_test_env() {
-    print "-> Creating the metro storage class"
-    kubectl apply -f ./resources/sc.yaml
+    print_notice "-> Creating the metro storage class"
+    kubectl apply -f $test_resources_path/$test_resource_sc
     if [ $? -ne 0 ]; then
-        echo "FAIL: Failed to create storage class"
+        print_fail "Failed to create storage class"
         return 1
     fi
 
-    print "-> Creating the test environment"
-    kubectl apply -f ./resources/deployment.yaml
+    print_notice "-> Creating the test environment"
+    kubectl apply -f $test_resources_path/$test_resource_deployment
     if [ $? -ne 0 ]; then
-        echo "FAIL: Failed to create test environment"
+        print_fail "Failed to create test environment"
         return 1
     fi
 }
@@ -95,7 +126,7 @@ function deploy_test_env() {
 # confirm a PV is provisioned and bound for the PVC.
 # status will be checked in $sleep_interval intervals for $timeout_seconds
 function test_pv_provisioning() {
-    print "-> Waiting for PV to be provisioned and PVC to bind..."
+    print_notice "-> Waiting for PV to be provisioned and PVC to bind..."
 
     # Check the PVC until a PV is provisioned
     start_time=$(date +%s)
@@ -105,7 +136,7 @@ function test_pv_provisioning() {
 
         # Check if a PV is provisioned
         if [[ "$pvc_status" == "Bound" ]]; then
-            echo "PASS: PV is provisioned for the PVC"
+            print_pass "PV is provisioned for the PVC"
             break
         fi
 
@@ -127,7 +158,7 @@ function test_pv_provisioning() {
 # 
 function test_pod_running() {
 
-    print "-> Waiting for pod $pod_name to be ready..."
+    print_notice "-> Waiting for pod $pod_name to be ready..."
 
     while true; do
         # Get the PVC status
@@ -135,7 +166,7 @@ function test_pod_running() {
 
         # Check if a PV is provisioned
         if [[ "$pod_status" == "Running" ]]; then
-            echo "PASS: Pod $pod_name is ready"
+            print_pass "Pod $pod_name is ready"
             break
         fi
 
@@ -168,7 +199,7 @@ function get_device_paths() {
 }
 
 function test_storage_multipath() {
-    print "-> Checking storage paths"
+    print_notice "-> Checking storage paths"
 
     local host_ip=$(kubectl get pod -n $namespace $pod_name -o jsonpath='{.status.hostIP}')
     local volume_name=$(kubectl get pvc -n $namespace $pvc_name -o jsonpath='{.spec.volumeName}')
@@ -178,7 +209,7 @@ function test_storage_multipath() {
 
     # confirm there are at least 4 mount points
     if [[ "${#mountpoints[@]}" -lt 4 ]]; then
-        echo "FAIL: Not enough mount paths found for volume '$volume_name'"
+        print_fail "Not enough mount paths found for volume '$volume_name'"
         echo "expected 4, found ${#mountpoints[@]}"
         return 1
     fi
@@ -189,15 +220,16 @@ function test_storage_multipath() {
         # confirm multipath provides at least 4 paths, 2 for each storage system in the metro configuration
         ssh_exec $host_ip 'multipathd show paths' | grep $device_path > /dev/null
         if [[ $? -ne 0 ]]; then
-            echo "FAIL: Could not find device path '$device_path' in multipath"
+            print_fail "Could not find device path '$device_path' in multipath"
             return 1
         fi
 
         echo "Host path for $device_path exists"
     done
+    print_pass "All device paths exist"
 
     # confirm the paths are in an 'active ready' state
-    print "-> Waiting for paths to be 'active ready'"
+    print_notice "-> Waiting for paths to be 'active ready'"
     while true; do
         local is_ready=0
 
@@ -213,7 +245,7 @@ function test_storage_multipath() {
             fi
         done
         if [[ "$is_ready" -eq 1 ]]; then
-            echo "PASS: All paths are 'active ready'"
+            print_pass "All paths are 'active ready'"
             break
         fi
 
@@ -241,15 +273,16 @@ function test_volume_availability() {
     local host_ip=$(kubectl get pod -n $namespace $pod_name -o jsonpath='{.status.hostIP}')
     local volume_name=$(kubectl get pvc -n $namespace $pvc_name -o jsonpath='{.spec.volumeName}')
 
-    print "-> Checking file system is writable"
+    print_notice "-> Checking file system is writable"
     write_to_pod_fs "test-sanity.txt"
     if [[ $? -ne 0 ]]; then
-        echo "FAIL: Failed to write to volume"
+        print_fail "Failed to write to volume"
         return 1
     fi
+    print_pass "File system is writable"
 
     # determine which pair of paths belong to the same storage array.
-    print "-> Getting LUN for device paths"
+    print_notice "-> Getting LUN for device paths"
 
     local -a device_paths=($(get_device_paths $host_ip $volume_name))
     declare -A LUN_IDS
@@ -261,22 +294,22 @@ function test_volume_availability() {
     done
 
     # use LUN to take down one side of the metro replication session
-    print "-> Taking down one side of the metro replication session"
+    print_notice "-> Taking down one side of the metro replication session"
     local -a paths_to_take_down=($(ssh_exec $host_ip "lsscsi \":::${LUN_IDS[${device_paths[0]}]}\"" | awk '{print $6}' | grep -E 'sd[a-z]*' | awk -F/ '{print $3}'))
     for path in "${paths_to_take_down[@]}"; do
         echo "Taking down $path"
         local response=$(ssh_exec $host_ip "multipathd fail path $path")
         if [[ "$response" != "ok" ]]; then
-            echo "FAIL: Could not take down $path"
+            print_fail "Could not take down $path"
             return 1
         fi
 
-        print "-> Waiting for paths to transition to 'failed' state"
+        print_notice "-> Waiting for paths to transition to 'failed' state"
         while true; do
 
             ssh_exec $host_ip 'multipathd show paths' | grep $path | grep 'failed faulty' > /dev/null
             if [[ $? -eq 0 ]]; then
-                echo "PASS: $path was taken down"
+                print_pass "$path was taken down"
                 break
             fi
 
@@ -294,13 +327,15 @@ function test_volume_availability() {
     done
 
     # confirm the volume mounted in the pod is still writable. do a few writes.
-    print "-> Confirming the file system is still writable"
+    print_notice "-> Confirming the file system is still writable"
     for i in {1..10}; do
         echo "Writing to volume. Iteration $i"
         write_to_pod_fs "test-sanity-$i.txt"
         if [[ $? -ne 0 ]]; then
-            echo "FAIL: Failed to write to volume"
+            print_fail "Failed to write to volume"
             return 1
+        else
+            print_pass "File system is writable: iteration $i"
         fi
 
         echo "Sleeping for 5 seconds"
@@ -308,13 +343,13 @@ function test_volume_availability() {
     done
 
     # restore the first side.
-    print "-> Restoring failed paths"
+    print_notice "-> Restoring failed paths"
     for path in "${paths_to_take_down[@]}"; do
         echo "Restoring path '$path'"
 
         local response=$(ssh_exec $host_ip "multipathd reinstate path $path")
         if [[ "$response" != "ok" ]]; then
-            echo "FAIL: Could not restore $path"
+            print_fail "Could not restore $path"
             return 1
         fi
     done
@@ -325,7 +360,7 @@ function test_volume_availability() {
 # PROVISIONING TEST
 # Confirm a metro PV is provisioned given a valid metro storage class and pvc.
 function test_suite_provisioning() {
-    print "-> Starting Provisioning test"
+    print_notice "-> Starting Provisioning test"
 
     deploy_test_env
     test_pv_provisioning
@@ -338,7 +373,7 @@ function test_suite_provisioning() {
 # Confirm the PV is setup on the host kubernetes node with multiple paths
 # to both the local and remote storage system.
 function test_suite_host_path() {
-    print "-> Starting Host Path test"
+    print_notice "-> Starting Host Path test"
 
     # confirm PV for PVC is created and bound and get the PV name.
     # confirm Pod is running.
@@ -355,7 +390,7 @@ function test_suite_host_path() {
 # Confirm the volume is still writable after taking down all paths to one
 # side of the metro replication session.
 function test_suite_volume_availability() {
-    print "-> Starting Volume Availability test"
+    print_notice "-> Starting Volume Availability test"
 
     # confirm the volume mounted in the pod is writable.
     test_pv_provisioning
@@ -368,14 +403,14 @@ function test_suite_volume_availability() {
 function clean_test_env() {
     pv_name=$(kubectl get pvc $pvc_name -n $namespace -o jsonpath='{.spec.volumeName}')
 
-    print "-> Deleting artifacts in namespace $namespace"
+    print_notice "-> Deleting artifacts in namespace $namespace"
     kubectl delete ns $namespace --wait=false
     
-    print "-> Waiting for namespace '$namespace' to be deleted"
+    print_notice "-> Waiting for namespace '$namespace' to be deleted"
     while true; do
         kubectl get ns $namespace > /dev/null
         if [[ $? != 0 ]]; then
-            echo "PASS: namespace '$namespace' successfully deleted"
+            print_pass "namespace '$namespace' successfully deleted"
             break
         fi
 
@@ -388,11 +423,11 @@ function clean_test_env() {
         fi
     done
 
-    print "-> Waiting for PV '$pv_name' to be deleted"
+    print_notice "-> Waiting for PV '$pv_name' to be deleted"
     while true; do
         kubectl get pv $pv_name > /dev/null
         if [[ $? != 0 ]]; then
-            echo "PASS: PV '$pv_name' successfully deleted"
+            print_pass "PV '$pv_name' successfully deleted"
             break
         fi
 
