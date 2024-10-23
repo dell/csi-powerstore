fc8ce02683c165dcefbf21726b2dee8952498e66
lukeatdell <115811384+lukeatdell@users.noreply.github.com>
2024-10-23 15:57:27 -0500

    Initialize hosts during dynamic secret update

--- cmd/csi-powerstore/main.go
+++ cmd/csi-powerstore/main.go
@@ -154,6 +154,10 @@ func main() {
 			if err != nil {
 				log.Fatalf("couldn't initialize arrays in node service: %s", err.Error())
 			}
+			err = nodeService.Init()
+			if err != nil {
+				log.Fatalf("couldn't create node service: %s", err.Error())
+			}
 		}
 	})
 
