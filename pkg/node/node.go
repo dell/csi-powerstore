c987a62ffa5f81cbd4f4d56b30d160d255247e14
sakshi-garg1 <74704849+sakshi-garg1@users.noreply.github.com>
2025-02-12 17:41:21 +0530

    Passing address.Portal instead of ipAddress in DiscoverTargetsWithInterface

--- pkg/node/node.go
+++ pkg/node/node.go
@@ -1248,7 +1248,7 @@ func (s *Service) NodeGetInfo(ctx context.Context, _ *csi.NodeGetInfoRequest) (*
 							log.Errorf("couldn't get interface: %s", err.Error())
 							continue
 						}
-						iscsiTargets, err = s.iscsiLib.DiscoverTargetsWithInterface(ipAddress, ipInterface[ipAddress], false)
+						iscsiTargets, err = s.iscsiLib.DiscoverTargetsWithInterface(address.Portal, ipInterface[ipAddress], false)
 						if err != nil {
 							log.Error("couldn't discover targets")
 							continue
