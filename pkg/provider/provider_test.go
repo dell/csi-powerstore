09b0f146ba274436af334aa3b42c56fe6b18225a
Harshita Pandey <Harshita_Pandey@dell.com>
2025-04-23 21:56:32 +0530

    Adding Unit tests

--- pkg/provider/provider_test.go
+++ pkg/provider/provider_test.go
@@ -14,6 +14,7 @@
 package provider
 
 import (
+	"os"
 	"testing"
 
 	"github.com/dell/csi-powerstore/v2/pkg/controller"
@@ -23,10 +24,77 @@ import (
 )
 
 func TestNew(t *testing.T) {
-	controllerSvc := controller.Service{}
-	identitySvc := identity.Service{}
-	nodeSvc := node.Service{}
 
-	p := New(&controllerSvc, &identitySvc, &nodeSvc, nil)
-	assert.NotNil(t, p)
+	tests := []struct {
+		name                string
+		setEnv              func()
+		unsetEnv            func()
+		createNamespaceFile func()
+		deleteNamespaceFile func()
+	}{
+		{
+			name:   "X_CSI_DRIVER_NAMESPACE environment variable is not set",
+			setEnv: func() {},
+			unsetEnv: func() {},
+			createNamespaceFile: func() {
+				err := os.MkdirAll("/var/run/secrets/kubernetes.io/serviceaccount", 0o755)
+				if err != nil {
+					t.Error(err)
+				}
+				file, err := os.Create(namespaceFile)
+				if err != nil {
+					t.Errorf("error creating file: %v", err)
+					t.Error(err)
+				}
+				defer func(file *os.File) {
+					err := file.Close()
+					if err != nil {
+						t.Error(err)
+					}
+				}(file)
+
+				_, err = file.Write([]byte("powerstore"))
+				if err != nil {
+					t.Error(err)
+				}
+			},
+			deleteNamespaceFile: func() {
+				err := os.Remove(namespaceFile)
+				if err != nil {
+					t.Error(err)
+				}
+			},
+		},
+		{
+			name: "X_CSI_DRIVER_NAMESPACE environment variable is set",
+			setEnv: func() {
+				err := os.Setenv("X_CSI_DRIVER_NAMESPACE", "powerstore")
+				if err != nil {
+					t.Error(err)
+				}
+			},
+			unsetEnv: func() {
+				err := os.Unsetenv("X_CSI_DRIVER_NAMESPACE")
+				if err != nil {
+					t.Error(err)
+				}
+			},
+			createNamespaceFile: func() {},
+			deleteNamespaceFile: func() {},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			controllerSvc := controller.Service{}
+			identitySvc := identity.Service{}
+			nodeSvc := node.Service{}
+			tt.setEnv()
+			tt.createNamespaceFile()
+			p := New(&controllerSvc, &identitySvc, &nodeSvc, nil)
+			tt.deleteNamespaceFile()
+			tt.unsetEnv()
+			assert.NotNil(t, p)
+		})
+	}
 }
