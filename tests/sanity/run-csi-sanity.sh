fbf99b4286fccd8efbbfbefb9a534d05dde42904
JacobGros <jacobgrosner4@gmail.com>
2025-06-04 16:31:51 -0400

    remove left over file from uncompleted prev. run

--- tests/sanity/run-csi-sanity.sh
+++ tests/sanity/run-csi-sanity.sh
@@ -1,6 +1,8 @@
 
 # remove left over from any uncompleted previous run
 rm -rf /tmp/csi-mount
+rm -rf /tmp/csi-staging
+
 
 csi-sanity --ginkgo.v --csi.controllerendpoint=controller.sock --csi.endpoint=node.sock --csi.testvolumeparameters=params.yaml --ginkgo.junit-report=report.xml 
 
