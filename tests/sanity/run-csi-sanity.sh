7313976cd127308c90fff087c718b27709ec0fe1
JacobGros <jacobgrosner4@gmail.com>
2025-06-04 16:29:13 -0400

    remove left over file from uncompleted prev. run

--- tests/sanity/run-csi-sanity.sh
+++ tests/sanity/run-csi-sanity.sh
@@ -1,3 +1,7 @@
+
+# remove left over from any uncompleted previous run
+rm -rf /tmp/csi-mount
+
 csi-sanity --ginkgo.v --csi.controllerendpoint=controller.sock --csi.endpoint=node.sock --csi.testvolumeparameters=params.yaml --ginkgo.junit-report=report.xml 
 
 # to run specific tests, add optional focus arguments like so:
