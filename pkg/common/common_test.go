6f7073ee3f97b2045b2de815820a81c22c7028a2
karthikk92 <karthik.k1@dell.com>
2025-06-25 10:58:41 +0530

    UT changes for review comments

--- pkg/common/common_test.go
+++ pkg/common/common_test.go
@@ -558,42 +558,28 @@ func TestGetMountFlags(t *testing.T) {
 func TestIsNFSServiceEnabled(t *testing.T) {
 	// Define mock client
 	clientMock := new(gopowerstoremock.Client)
+	// Initialise variable for nas servers
+	nasServers := []gopowerstore.NAS{
+		{
+		NfsServers: []gopowerstore.NFSServerInstance{
+		{
+		ID: "4444",
+		IsNFSv4Enabled: true,
+		},
+		},
+		},
+		}
 
 	// Test cases
 	t.Run("nfs service is enabled", func(t *testing.T) {
-		clientMock.On("GetNASServers", mock.Anything, mock.Anything).Return(
-			[]gopowerstore.NAS{
-				{
-					NfsServers: []gopowerstore.NFSServerInstance{
-						{
-							ID:             "4444",
-							IsNFSv4Enabled: true,
-						},
-					},
-				},
-			},
-			nil,
-		)
-
+		clientMock.On("GetNASServers", mock.Anything, mock.Anything).Return(nasServers, nil)
 		_, err := common.IsNFSServiceEnabled(context.Background(), clientMock)
 		assert.NoError(t, err)
 	})
 
 	t.Run("nfs service is not enabled", func(t *testing.T) {
-		clientMock.On("GetNASServers", mock.Anything, mock.Anything).Return(
-			[]gopowerstore.NAS{
-				{
-					NfsServers: []gopowerstore.NFSServerInstance{
-						{
-							ID:             "4444",
-							IsNFSv4Enabled: false,
-						},
-					},
-				},
-			},
-			nil,
-		)
-
+		nasServers[0].NfsServers[0].IsNFSv4Enabled = false
+		clientMock.On("GetNASServers", mock.Anything, mock.Anything).Return(nasServers, nil)
 		_, err := common.IsNFSServiceEnabled(context.Background(), clientMock)
 		assert.NoError(t, err)
 	})
