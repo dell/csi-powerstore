599d3827815eb949c2c9f250872679194bfd0213
karthikk92 <karthik.k1@dell.com>
2025-06-26 10:38:01 +0530

    Added scenario for negetive tc for nfs label

--- pkg/node/node_test.go
+++ pkg/node/node_test.go
@@ -5048,6 +5048,45 @@ var _ = ginkgo.Describe("CSINodeService", func() {
 			})
 		})
 	})
+	ginkgo.Describe("calling NodeGetInfo() when nfs is disabled", func() {
+		ginkgo.When("managing multiple arrays", func() {
+			ginkgo.It("should return correct topology segments", func() {
+				nasData[0].NfsServers[0].IsNFSv4Enabled = false
+				nasData[0].NfsServers[0].IsNFSv3Enabled = false
+				clientMock.On("GetNASServers", mock.Anything).
+					Return(nasData, nil)
+				clientMock.On("GetStorageISCSITargetAddresses", mock.Anything).
+					Return([]gopowerstore.IPPoolAddress{
+						{
+							Address: "192.168.1.1",
+							IPPort:  gopowerstore.IPPortInstance{TargetIqn: "iqn"},
+						},
+						{
+							Address: "192.168.1.2",
+							IPPort:  gopowerstore.IPPortInstance{TargetIqn: "iqn2"},
+						},
+					}, nil)
+				conn, _ := net.Dial("udp", "127.0.0.1:80")
+				fsMock.On("NetDial", mock.Anything).Return(
+					conn,
+					nil,
+				)
+				setDefaultNodeLabelsMock()
+
+				res, err := nodeSvc.NodeGetInfo(context.Background(), &csi.NodeGetInfoRequest{})
+				gomega.Expect(err).To(gomega.BeNil())
+				gomega.Expect(res).To(gomega.Equal(&csi.NodeGetInfoResponse{
+					NodeId: nodeSvc.nodeID,
+					AccessibleTopology: &csi.Topology{
+						Segments: map[string]string{
+							common.Name + "/" + firstValidIP + "-iscsi": "true",
+						},
+					},
+					MaxVolumesPerNode: 0,
+				}))
+			})
+		})
+	})
 })
 
 func TestInitConnectors(t *testing.T) {
