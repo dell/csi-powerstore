1364f31a92662a232b69de35dda41d0497957902
Luna Xu <10015938+xuluna@users.noreply.github.com>
2025-07-02 10:24:37 -0500

    Remove sharednfs codes that starts node monitor services that affects performance (#509)

--- pkg/service/service.go
+++ pkg/service/service.go
@@ -59,7 +59,7 @@ func New() nfs.Service {
 	return &service{}
 }
 
-func (s *service) BeforeServe(ctx context.Context, sp *gocsi.StoragePlugin, lis net.Listener) error {
+func (s *service) BeforeServe(ctx context.Context, _ *gocsi.StoragePlugin, _ net.Listener) error {
 	log.Info("-----Inside Before Serve-----")
 	// Get the SP's operating mode.
 	s.mode = csictx.Getenv(ctx, gocsi.EnvVarMode)
@@ -82,19 +82,21 @@ func (s *service) BeforeServe(ctx context.Context, sp *gocsi.StoragePlugin, lis 
 		nfs.NodeRoot = nodeRoot
 	}
 
+	log.Infof("Setting node name env to %s for NFS", nodeName)
 	err := os.Setenv("X_CSI_NODE_NAME", nodeName)
 	if err != nil {
 		log.Errorf("failed to set env X_CSI_NODE_NAME. err: %s", err.Error())
 		return err
 	}
 
-	log.Infof("Setting node name env to %s for NFS", nodeName)
-
-	err = nfssvc.BeforeServe(ctx, sp, lis)
-	if err != nil {
-		log.Errorf("unable to start up nfsserver: %s", err.Error())
-	}
-
+	// The block is commented out for performance issue caused by sharednfs even when it's disabled.
+	// Remove the comment when enabling sharednfs feature.
+	/*
+		err = nfssvc.BeforeServe(ctx, sp, lis)
+		if err != nil {
+			log.Errorf("unable to start up nfsserver: %s", err.Error())
+		}
+	*/
 	return nil
 }
 
